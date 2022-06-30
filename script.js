7f1e566d81a4cda64af14214104e350c1a9cbaa8
Navneesh Yadav <66569907+NavneeshYadav1@users.noreply.github.com>
2022-06-30 16:45:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const calculateTemp= () =>{
+    const numberTemp=document.getElementById("temp").value;
+    const tempSelected =document.getElementById("temp_diff");
+    const valueTemp=tempSelected.options[tempSelected.selectedIndex].value;
+    
+
+    const celToFah = (cel) =>{
+        let fahrenheit =(cel* 9/5) +32;
+        return fahrenheit;
+    }
+    const fahToCel= (fah)=>{
+        let celsius =((fah-32)*5/9);
+        return celsius;
+    }
+    let result;
+
+    if(valueTemp=="cel"){
+        result=celToFah(numberTemp);
+        document.getElementById("resultContainer").innerHTML=` Result : ${numberTemp} °Celsius= ${result} °Fahrenheit`;
+    }
+    else{
+        result=fahToCel(numberTemp);
+        document.getElementById("resultContainer").innerHTML=`Result : ${numberTemp} °Fahrenheit= ${result} °Celsius`;
+    }
+}
